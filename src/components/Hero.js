721342556f4131c8dc9083f3e1e8750f8757472c
Bryan Ge <bryange3@gmail.com>
2020-12-31 16:27:20 -0600

    Update logo

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -13,7 +13,7 @@ function Hero(props) {
                         { props.title && <h1 className="display-2 font-weight-bolder">{props.title}</h1> }
                         { props.subTitle && <h2 className="display-4 font-weight-normal">{props.subTitle}</h2> }
                         { props.projectName && <h2 className="display-4 font-weight-bolder">{props.projectName}</h2> }
-                        { props.description && <h3 className="display-5 font-weight-light">{props.description}</h3> }
+                        { props.description && <h3 className="display-5 font-weight-light project-description">{props.description}</h3> }
                         { props.small && <h3 className="font-weight-normal text-muted mt-4 small-description">{props.small}</h3> }
                     </Col>
                 </Row>
