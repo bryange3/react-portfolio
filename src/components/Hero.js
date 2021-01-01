95cf2f698e7bc4cc4f3f52fdf5f6eea44edf15ea
Bryan Ge <bryange3@gmail.com>
2021-01-01 14:58:44 -0600

    Update styling

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -24,8 +24,8 @@ function Hero(props) {
                         {props.animated && <h1 className="display-1"><Animation>{props.animated}</Animation></h1>}
                         {props.title && <h1 className="display-2 font-weight-bolder">{props.title}</h1>}
                         {props.subTitle && <h2 className="display-4 font-weight-normal">{props.subTitle}</h2>}
-                        {props.projectName && <h2 className="display-4 font-weight-bolder">{props.pnpmrojectName}</h2>}
-                        {props.description && <h3 className="display-5 font-weight-light project-description">{props.description}</h3>}
+                        {props.projectName && <h2 className="display-4 font-weight-bolder">{props.projectName}</h2>}
+                        {props.description && <h3 className="display-5 font-weight-normal project-description">{props.description}</h3>}
                         {props.small && <h3 className="font-weight-normal text-muted mt-4 small-description">{props.small}</h3>}
                     </Col>
                 </Row>
