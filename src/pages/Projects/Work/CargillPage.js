e87c86d7d1db589ca2fef0d204074a496c2475cc
Bryan Ge <bryange3@gmail.com>
2021-07-07 14:35:22 -0500

    Update Cargill page

--- src/pages/Projects/Work/CargillPage.js
+++ src/pages/Projects/Work/CargillPage.js
@@ -18,25 +18,42 @@ function CargillPage(props) {
                         <br />
                         <b>Timeline</b>: {props.info.timeline}
                         <br />
+                        <b>Team Members</b>: {props.info.teamMembers}
+                        <br />
                         <b>Design Tools</b>: Figma, FigJam
                     </p>
 
                     <hr class="rounded" />
 
-                    <p>
-                        As the sole UX/UI Design Intern in my project team at Cargill, I lead our product's discovery, user research, wireframing, and prototyping efforts. The internship has provided me the opportunity to directly interact with clients through demos, discovery sessions, and shadowing sessions. These meetings have helped me identify our problem space and what the client finds most valuable.
+                    <p className="paragraph">
+                        <p className="section">
+                            Summary
+                        </p>
+                        As the <b>sole designer</b> in my project team at Cargill, I lead our product's discovery, user research, wireframing, and prototyping efforts. The internship has provided me the opportunity to directly interact with clients through discovery and shadowing sessions, demos, and feedback calls. These meetings  helped me identify our problem space as well as what the client values.
+                        <p className="paragraph">
+                            I am unable to display my work due to <b>NDA restrictions</b>, so please contact me at <a className="link-underline" href="mailto:bryange3@gmail.com">bryange3@gmail.com</a> if you want to find out more!
+                        </p>
                     </p>
 
-                    <p>
-                        I've also learned how to collaborate and communicate with software engineers through daily standup, clarifications regarding designs, creating a style sheet, and utilizing Material UI and React components for easy handoff and quick developer prototyping.
-                    </p>
 
-                    <p>
-                        In fact, I also ran a mini design sprint with the developers in my team to view our product and problem space from a variety of angles. Collectively writing out how-might-we statements and noticing the similarities and differences in them helped us discover edge cases and potential features for our dashboard that could greatly benefit our target users.
-                    </p>
 
-                    <p>
-                        More details to come!
+                    <p className="paragraph">
+                        <p className="section">
+                            Reflections
+                        </p>
+
+                        <h3 className="subsection">
+                            I've learned how to collaborate and communicate within a cross-functional team in an Agile environment.
+                        </h3>
+                        This included daily standup, clarifications regarding designs, creating a style sheet, and utilizing Material UI and React components for easy handoff and quick developer prototyping.
+
+                        <h3 className="subsection">
+                            Being open-minded and maintaining a culture where all team members are involved increases communication and efficiency.
+                        </h3>
+
+                        <p className="paragraph">
+                            I ran a mini design sprint with the developers in my team to view our product and problem space from a variety of angles. Collectively writing out how-might-we statements and noticing the similarities and differences in them helped us discover edge cases and potential features for our dashboard that could greatly benefit our target users.
+                        </p>
                     </p>
                 </body>
             </Content>
